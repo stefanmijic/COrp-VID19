b5c4c51e5f07bd27ed12cab1b2021b84839cfc41
LimeThaw <laudit@student.ethz.ch>
2020-03-28 08:42:35 +0100

    Added example template, finished POC for data display

--- corp/corpapp/models.py
+++ corp/corpapp/models.py
@@ -4,3 +4,6 @@ class University(models.Model):
     name = models.CharField(max_length=128)
     nstudents = models.IntegerField()
     nstaff = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- corp/corpapp/views.py
+++ corp/corpapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import University
+from django.db.models.functions import Lower
 
 def index(request):
-    return HttpResponse("hello, world.")
+    unilist = University.objects.order_by(Lower('name'))
+    context = {'unilist': unilist}
+    return render(request, 'corpapp/index.html', context)
