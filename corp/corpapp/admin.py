e76a8c11c96db1a47d97a4b8e6252987ce649e53
jkirsch <jkirsch@student.ethz.ch>
2020-03-28 14:16:54 +0100

    Granted Admin Privileges

--- corp/corpapp/admin.py
+++ corp/corpapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import University
+from .models import University, State, Country
 
 admin.site.register(University)
+admin.site.register(State)
+admin.site.register(Country)
