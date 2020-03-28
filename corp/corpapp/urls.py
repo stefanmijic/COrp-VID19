e79447e8902b6040cffd10aa6520b493eedacc10
LimeThaw <laudit@student.ethz.ch>
2020-03-28 20:06:07 +0100

    Finished list views

--- corp/corpapp/urls.py
+++ corp/corpapp/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
         path("", views.index, name="index"),
+        path("countries", views.countries, name="countries"),
+        path("states", views.states, name="states"),
+        path("cities", views.cities, name="cities"),
+        path("universities", views.universities, name="universities"),
+        path("corporations", views.corporations, name="corporations"),
     ]
--- corp/corpapp/views.py
+++ corp/corpapp/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
-from .models import University
+from .models import Country, State, City, University, Corporation
 from django.db.models.functions import Lower
 
 def index(request):
+    return render(request, 'corpapp/index.html')
+
+def countries(request):
+    cntlist = Country.objects.order_by(Lower('name'))
+    context = {'cntlist': cntlist}
+    return render(request, 'corpapp/countries.html', context)
+
+def states(request):
+    stlist = State.objects.order_by(Lower('name'))
+    context = {'stlist': stlist}
+    return render(request, 'corpapp/states.html', context)
+
+def cities(request):
+    ctylist = City.objects.order_by(Lower('name'))
+    context = {'ctylist': ctylist}
+    return render(request, 'corpapp/cities.html', context)
+
+def universities(request):
     unilist = University.objects.order_by(Lower('name'))
     context = {'unilist': unilist}
-    return render(request, 'corpapp/index.html', context)
+    return render(request, 'corpapp/universities.html', context)
+
+
+def corporations(request):
+    corplist = Corporation.objects.order_by(Lower('name'))
+    context = {'corplist': corplist}
+    return render(request, 'corpapp/corporations.html', context)
