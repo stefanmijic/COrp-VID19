c7e537ee6c2a11088d09d92b6d7d5a9d2d728e72
jkirsch <jkirsch@student.ethz.ch>
2020-03-28 16:48:04 +0100

    Feed the database
    write python manage.py loaddata states.json and then python manage.py loaddata universities.json

--- corp/corpapp/models.py
+++ corp/corpapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class University(models.Model):
     name = models.CharField(max_length=128)
     abbreviation = models.CharField(max_length=10)
+    kind = models.CharField(max_length=32)
     ownership = models.CharField(max_length=10)
     n_students = models.IntegerField()
     n_staff = models.IntegerField()
@@ -11,7 +12,7 @@ class University(models.Model):
     city = models.CharField(max_length=64)
     budget = models.IntegerField()
     corona_page = models.CharField(max_length=600)
-    main_page = models.CharField(max_length=12)
+    main_page = models.CharField(max_length=128)
     quality = models.CharField(max_length=12)
 
     def __str__(self):
@@ -20,10 +21,9 @@ class University(models.Model):
 
 class State(models.Model):
     name = models.CharField(max_length=128)
-    abbreviation = models.CharField(max_length=5)
+    abbreviation = models.CharField(max_length=8)
     country = models.ForeignKey('Country', on_delete=models.CASCADE,)
-    bip = models.IntegerField()
-
+    
     def __str__(self):
         return self.abbreviation
 
